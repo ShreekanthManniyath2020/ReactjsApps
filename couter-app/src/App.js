c3c42e11bbdc464d5bf6afdf8dc03a1f8baf2ea3
shreekanth <shreekanth0006@gmail.com>
2021-12-27 15:53:13 +0530

    counter App

---
+++ couter-app/src/App.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import Counter from "./Counter";
+
+function App() {
+  const [count,setCount] = useState(0)
+  const addCount=()=>{
+    setCount(count+1)
+
+  }
+  return (
+    <div className="App">
+    <button onClick={addCount}>Add</button>
+    <Counter count={count}/>
+    </div>
+  );
+}
+
+export default App;
