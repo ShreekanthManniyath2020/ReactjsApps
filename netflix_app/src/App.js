ace3020dadfef4e82491ab7ef438574cc98f4714
shreekanth0006 <shreekanth0006@gmail.com>
2021-12-31 19:34:28 +0530

    Pulling data from Rest services and Updating in Netflix app
    from TMDB services and axios , youtube react library

--- netflix_app/src/App.js
+++ netflix_app/src/App.js
@@ -3,12 +3,16 @@ import NavBar from "./Components/NavBar/NavBar";
 import './App.css';
 import Banner from "./Components/Banner/Banner";
 import RowPost from "./Components/RowPost/RowPost";
+import {originals,action,comendy} from "./urls"
+
 function App() {
   return (
     <div className="App">
       <NavBar/>
       <Banner/>
-      <RowPost/>
+      <RowPost url={originals} title='Netflix Originals'/>
+      <RowPost url={action} title='Action' isSmall/>
+      <RowPost url={comendy} title='Comendy' isSmall/>
     </div>
   );
 }
---
+++ netflix_app/src/constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl='https://api.themoviedb.org/3';
+export const API_KEY='1eb4e01cbea2ce4a31f02e6984f21345';
+export const imageUrl = 'https://image.tmdb.org/t/p/original'
