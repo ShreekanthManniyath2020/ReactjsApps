2d29e9d20fd0dc3b1e0b3d6511d9a73d94feeb9e
shreekanth <shreekanth0006@gmail.com>
2025-06-30 16:08:17 +0530

    Adding bootstrap header

---
+++ samplereactapp/src/App.jsx
@@ -0,0 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+
+import AppHeader from './components/header';
+
+
+function App() {
+  return (
+    <div className='App'>
+      <header id='header'>
+<AppHeader/>
+      </header>
+    </div>
+  )
+}
+
+export default App
